1909c88045dec5418b0eb41a6884c91406c935fd
theowner150 <60976308+theowner150@users.noreply.github.com>
2020-02-12 10:22:19 -0500

    Initial commit

---
+++ Assets/player.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    public float movespeed = 600f;
+
+    float movement = 0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+       movement = Input.GetAxisRaw("Horizontal");   
+    }
+    private void FixedUpdate()
+    {
+      
+    }
+}
---
+++ Assets/scripts/doorscript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class doorscript : MonoBehaviour
+{
+    public int timer = 0;
+    public GameObject door;
+    public Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
